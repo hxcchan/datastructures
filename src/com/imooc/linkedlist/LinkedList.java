0894249d0043ff657e57187f1658e401b73fb40f
hs <hansong9547@gmail.com>
2020-10-14 02:30:00 -0400

    use dummyhead

--- src/com/imooc/linkedlist/LinkedList.java
+++ src/com/imooc/linkedlist/LinkedList.java
@@ -2,35 +2,33 @@ package com.imooc.linkedlist;
 
 public class LinkedList<E> {
     private class Node{
-        public E e;
-        public Node next;
+    public E e;
+    public Node next;
 
-        public Node(E e, Node next){
-            this.e = e;
-            this.next = next;
-        }
-
-        public Node(E e){
-            this.e = e;
-            next = null;
-        }
-
-        public Node(){
-            this(null, null);
-        }
-
-        @Override
-        public String toString() {
-            return e.toString();
-        }
+    public Node(E e, Node next){
+        this.e = e;
+        this.next = next;
     }
 
-    private Node head;
+    public Node(E e){
+        this.e = e;
+        next = null;
+    }
+
+    public Node(){
+        this(null, null);
+    }
+
+    @Override
+    public String toString() {
+        return e.toString();
+    }
+}
+
+    private Node dummyHead;
     private int size;
-
-
     public LinkedList(){
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -46,25 +44,25 @@ public class LinkedList<E> {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Index is not correct");
         }
-        if (index == 0) {
-            addFirst(e);
-        } else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
-                prev = prev.next;
-            }
 
-            prev.next = new Node(e, prev.next);
-            size++;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
 
-    }
-    public void addFirst(E e){
-        head = new Node(e, head);
+        prev.next = new Node(e, prev.next);
         size++;
+
+
+    }
+
+    public void addFirst(E e){
+        add(0, e);
     }
 
     public void addLast(E e) {
         add(size, e);
     }
+
+
 }
