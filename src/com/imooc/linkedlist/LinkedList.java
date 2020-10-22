b6b7b9b5f6a2d73e9d30f4f38825e59f80c55598
DanielHan <han.son@husky.neu.edu>
2020-10-22 03:48:02 -0400

    LinkedListQueue

--- src/com/imooc/linkedlist/LinkedList.java
+++ src/com/imooc/linkedlist/LinkedList.java
@@ -1,29 +1,30 @@
 package com.imooc.linkedlist;
 
 public class LinkedList<E> {
-    private class Node{
-    public E e;
-    public Node next;
+    private class Node {
+        public E e;
+        public Node next;
 
-    public Node(E e, Node next){
-        this.e = e;
-        this.next = next;
-    }
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
 
-    public Node(E e){
-        this.e = e;
-        next = null;
-    }
+        public Node(E e) {
+            this.e = e;
+            next = null;
+        }
 
-    public Node(){
-        this(null, null);
-    }
+        public Node() {
+            this(null, null);
+        }
 
-    @Override
-    public String toString() {
-        return e.toString();
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+
     }
-}
 
     private Node dummyHead;
     private int size;
